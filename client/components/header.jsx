54b066ab2a64c62822710e8b087b3460ebbab334
edwardcolley <edward.colley@outlook.com>
2019-07-11 19:25:56 +0000

    added icon and font styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,7 +5,7 @@ export class Header extends React.Component {
   render() {
     return (
       <div className="row">
-        <h1 className="col display-2 font-weight-bold text-center mt-4">Wicked Sales</h1>
+        <h1 className="col display-2 font-weight-bold text-warning text-center mt-4"><img src="/images/shoppingcart.png"></img>Wicked Sales</h1>
       </div>
     );
   }
