45a5b2f47cb58cc1f04b6a915b5f8f9e648203cc
edwardcolley <edward.colley@outlook.com>
2019-10-04 21:53:37 +0000

    added new item pictures made the product list page much more responsive still need to make product details page responsive

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -6,11 +6,11 @@ export class ProductListItem extends React.Component {
   render() {
     const price = '$' + ((this.props.input.price / 100).toFixed(2));
     return (
-      <Col sm={{ size: 4 }} lg={{ size: 3 }} xl={{ size: 2 }} className=" cardStyling">
+      <Col xs={{ size: 10 }} sm={{ size: 4 }} lg={{ size: 3 }} xl={{ size: 2 }} className="mobileProductStyle cardStyling">
         <div className="card-group h-50">
           <div onClick={this.props.onClick} className="card shadow bg-dark rounded text-white" >
             <p className="card-header text-center cardHeaderStyle">{this.props.input.name}</p>
-            <img src={this.props.input.image} height="150" width="95%" className="card-img-top" alt="item image" />
+            <img src={this.props.input.image} className="productStyle card-img-top" alt="item image" />
             <div className="card-body">
               <h6 className="card-title text-center">{price}</h6>
               {/* <p className="card-text h-50">{this.props.input.shortDescription}</p> */}
